5155250a2b7e89dab544434279aae02c5d8e52f6
Aditya-Borhade <adityaborhade793@gmail.com>
2025-07-13 11:45:36 +0530

    updated root file

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const listings=require("./routes/listing.js");
 const reviews=require("./routes/review.js");
 const user=require("./routes/user.js");
 
-//const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+
 const DB_URL=process.env.ATLAS_URL;
 
 main()
@@ -44,24 +44,12 @@ app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
-// saving to database is a asynchronous thats why i use async-await 
-/*app.get("/testListing" , async(req,res)=>{
-    let sampleListing=new Listing({
-        title:"my new villa",
-        description:"By the beach",
-        price: 12000,
-        location: "marine Drive, Mumbai",
-        country:"India",
-    });
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("sucessful testing");
-}); */
+
 
 const store=MongoStore.create({
     mongoUrl:DB_URL,
     crypto:{
-        secret: "mysupersecretcode",
+        secret:process.env.SECRET,
     },
     touchAfter : 24*3600,
 });
@@ -72,7 +60,7 @@ store.on("error" , (err)=>{
 
 const sessionOptions={
     store,
-    secret: "mysupersecretcode",
+    secret:process.env.SECRET,
     resave: false,
     saveUninitialized : true,
     cookie:{
@@ -103,119 +91,14 @@ app.use((req,res,next)=>{
     next();
 });
 
-// app.get("/demouser" , async(req,res)=>{
-//     let fakeUser=new User({
-//         email:"student@gmail.com",
-//         username:"sigma-student",
-//     })
-//     let registerUser=await User.register(fakeUser,"helloworld");
-//     res.send(registerUser);
-// })
 
-/*
-const validateListing=(req,res,next)=>{
-    let {error} = listingSchema.validate(req.body);
-    if(error){
-    let errmsg=error.details.map((el)=>el.message.join(","));
-        throw new ExpressError(400, errmsg);
-    }
-    else{
-        next();
-    }
-}
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        // el.message is already a string
-        const errmsg = error.details.map(el => el.message).join(', ');
-        throw new ExpressError(400, errmsg);
-    } else {
-        next();
-    }
-};
-*/ 
-
-//INDEX ROUTE 
-// app.get('/listings',wrapAsync(async(req,res)=>{
-//     const allListings= await Listing.find({});
-//     res.render("./listings/index.ejs",{allListings});
-// }));
-
-/*
-//NEW ROUTE //
-app.get("/listings/new",(req,res)=>{
-    res.render("./listings/new.ejs");
-});
-
-// CREATE ROUTE //
-app.post("/listings",validateListing ,wrapAsync(async(req,res,next)=>{
-   // let {title,description,image,price,location,country} =req.body;
-   //const listing=req.body.listing;
-    
-    // if(!req.body.listing){
-    //     throw new ExpressError(400,"Enter a valid data");
-    // }
-    const newlisting=new Listing(req.body.listing);
-    await newlisting.save();
-    console.log(newlisting);
-    res.redirect("/listings");
-}));
-
-// SHOW ROUTE 
-app.get("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    const listings=await Listing.findById(id).populate("reviews");
-    res.render("./listings/show.ejs",{listings});
-}));
-
-//EDIT ROUTE //
-app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    const listing=await Listing.findById(id);
-    res.render("./listings/edit.ejs",{listing})
-}));
-
-// UPDATE ROUTE //
-app.put("/listings/:id",validateListing ,wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}));
-
-//DELETE ROUTE //
-app.delete("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id} =req.params;
-    const deleted=await Listing.findByIdAndDelete(id);
-    res.redirect("/listings");
-})); */
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
 app.use("/",user);
 
-/*
-// REVIEWS
-//POST ROUTE
-app.post("/listings/:id/reviews",validateReview, wrapAsync(async(req,res)=>{
-    let listing=await Listing.findById(req.params.id);
-    let newReview=new Review(req.body.review);
 
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-    
-    res.redirect(`/listings/${listing._id}`);
-}));
 
-// DELETE REVIEW ROUTE 
-app.delete("/listings/:id/reviews/:reviewID",wrapAsync(async(req,res)=>{
-     let {id, reviewID} =req.params;
-     await Listing.findByIdAndUpdate(id , {$pull :{review: reviewID}});
-     await Review.findByIdAndDelete(reviewID);
-     res.redirect(`/listings/${id}`);
-}));
- */
 
 app.use((req,res,next)=>{
    next(new ExpressError(404," this route dosent exist !"))
